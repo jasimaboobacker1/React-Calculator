9b3a9eeef08121db3bfe74993845f4672586309f
unknown <jasimaboobacker1>
2023-10-31 23:16:15 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import './App.css'
 function App() {
+
+  const [value,setValue] = useState("");
+
+  function handle(e) {
+    setValue(value + e.target.innerHTML)
+  }
+  function equal (e){
+    setValue(eval(value))
+  }
+ function Ac (e) {
+  setValue("")
+ }
+ function c (e) {
+  setValue(value.toString().slice(0,-1))
+ }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <center>
+      <h1>CALCULATOR</h1>
+       <div class="calculator">
+          <input type="text" value={value} placeholder='0'/>
+          <button class="btn" onClick={c}>C</button>
+          <button class="btn" onClick={Ac} >AC</button>
+          <button class="btn" id='equal ' onClick={equal}>=</button>
+          <button class="btn" onClick={handle}>1</button>
+          <button class="btn" onClick={handle}>2</button>
+          <button class="btn" onClick={handle}>3</button>
+          <button class="btn" onClick={handle}>4</button>
+          <button class="btn" onClick={handle}>5</button>
+          <button class="btn" onClick={handle}>6</button>
+          <button class="btn" onClick={handle}>7</button>
+          <button class="btn" onClick={handle}>8</button>
+          <button class="btn" onClick={handle}>9</button>
+          <button class="btn" onClick={handle}>+</button>
+          <button class="btn" onClick={handle}>-</button>
+          <button class="btn" onClick={handle}>*</button>
+         
+          <button class="btn" onClick={handle} >/</button>
+         
+      </div>
+    </center>
+    </>
+   
+  )
 }
 
-export default App;
+export default App
